671e400b0b46cbb7a224b5a79c477765221d858d
monan <651932351@qq.com>
2024-07-01 10:59:11 +0800

    Remove useless script

--- auto-translater.py
+++ auto-translater.py
@@ -8,192 +8,35 @@ from modelscope.utils.constant import Tasks
 import logging
 import tensorflow as tf
 
-logging.basicConfig(level=logging.ERROR) 
-
-# 设置最大输入字段，超出会拆分输入，防止超出输入字数限制
-max_length = 1800
+logging.basicConfig(level=logging.ERROR)
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1,"
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print("GPU: ", gpus)
-pipeline_ins = pipeline(task=Tasks.translation, model="damo/nlp_csanmt_translation_zh2en", device="cpu")
+pipeline_ins = pipeline(task=Tasks.translation, model="damo/nlp_csanmt_translation_zh2en", device="cuda")
 
 # 设置翻译的路径
 dir_to_translate = "testdir/to-translate"
 dir_translated = {
     "en": "testdir/docs/en",
-    "es": "testdir/docs/es",
-    "ar": "testdir/docs/ar"
 }
 
-# 不进行翻译的文件列表
-exclude_list = ["index.md", "Contact-and-Subscribe.md", "WeChat.md"]
-# 已处理的 Markdown 文件名的列表，会自动生成
 processed_list = "processed_list.txt"
 
-# 由 ChatGPT 翻译的提示
-tips_translated_by_chatgpt = {
-    "en": "\n\n> This post is translated using ChatGPT, please [**feedback**](https://github.com/linyuxuanlin/Wiki_MkDocs/issues/new) if any omissions.",
-    "es": "\n\n> Este post está traducido usando ChatGPT, por favor [**feedback**](https://github.com/linyuxuanlin/Wiki_MkDocs/issues/new) si hay alguna omisión.",
-    "ar": "\n\n> تمت ترجمة هذه المشاركة باستخدام ChatGPT، يرجى [**تزويدنا بتعليقاتكم**](https://github.com/linyuxuanlin/Wiki_MkDocs/issues/new) إذا كانت هناك أي حذف أو إهمال."
-}
-
-# 文章使用英文撰写的提示，避免本身为英文的文章被重复翻译为英文
-marker_written_in_en = "\n> This post was originally written in English.\n"
-# 即使在已处理的列表中，仍需要重新翻译的标记
-marker_force_translate = "\n[translate]\n"
-
-# Front Matter 处理规则
-front_matter_translation_rules = {
-    # 调用 ChatGPT 自动翻译
-    "title": lambda value, lang: translate_text(value, lang,"front-matter"),
-    "description": lambda value, lang: translate_text(value, lang,"front-matter"),
-    
-    # 使用固定的替换规则
-    "categories": lambda value, lang: front_matter_replace(value, lang),
-    "tags": lambda value, lang: front_matter_replace(value, lang),
-    
-    # 未添加的字段将默认不翻译
-}
-
-# 固定字段替换规则。文章中一些固定的字段，不需要每篇都进行翻译，且翻译结果可能不一致，所以直接替换掉。
-replace_rules = [
-    {
-        # 版权信息手动翻译
-        "orginal_text": "> 原文地址：<https://wiki-power.com/>",
-        "replaced_text": {
-            "en": "> Original: <https://wiki-power.com/>",
-            "es": "> Dirección original del artículo: <https://wiki-power.com/>",
-            "ar": "> عنوان النص: <https://wiki-power.com/>",
-        }
-    },
-    {
-        # 版权信息手动翻译
-        "orginal_text": "> 本篇文章受 [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by/4.0/deed.zh) 协议保护，转载请注明出处。",
-        "replaced_text": {
-            "en": "> This post is protected by [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by/4.0/deed.en) agreement, should be reproduced with attribution.",
-            "es": "> Este artículo está protegido por la licencia [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by/4.0/deed.zh). Si desea reproducirlo, por favor indique la fuente.",
-            "ar": "> يتم حماية هذا المقال بموجب اتفاقية [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by/4.0/deed.zh)، يُرجى ذكر المصدر عند إعادة النشر.",
-        }
-    },
-    {
-        # 文章中的站内链接，跳转为当前相同语言的网页
-        "orginal_text": "](https://wiki-power.com/",
-        "replaced_text": {
-            "en": "](https://wiki-power.com/en/",
-            "es": "](https://wiki-power.com/es/",
-            "ar": "](https://wiki-power.com/ar/",
-        }
-    }
-    # {
-    #    # 不同语言可使用不同图床
-    #    "orginal_text": "![](https://wiki-media-1253965369.cos.ap-guangzhou.myqcloud.com/",
-    #    "replaced_en": "![](https://f004.backblazeb2.com/file/wiki-media/",
-    #    "replaced_es": "![](https://f004.backblazeb2.com/file/wiki-media/",
-    #    "replaced_ar": "![](https://f004.backblazeb2.com/file/wiki-media/",
-    # },
-]
-
-# Front Matter 固定字段替换规则。
-front_matter_replace_rules = [
-    {
-        "orginal_text": "类别 1",
-        "replaced_text": {
-            "en": "Categories 1",
-            "es": "Categorías 1",
-            "ar": "الفئة 1",
-        }
-    },
-    {
-        "orginal_text": "类别 2",
-        "replaced_text": {
-            "en": "Categories 2",
-            "es": "Categorías 2",
-            "ar": "الفئة 2",
-        }
-    },
-    {
-        "orginal_text": "标签 1",
-        "replaced_text": {
-            "en": "Tags 1",
-            "es": "Etiquetas 1",
-            "ar": "بطاقة 1",
-        }
-    },
-    {
-        "orginal_text": "标签 2",
-        "replaced_text": {
-            "en": "Tags 2",
-            "es": "Etiquetas 2",
-            "ar": "بطاقة 2",
-        }
-    },
-]
-
-##############################
-
-# 对 Front Matter 使用固定规则替换的函数
-def front_matter_replace(value, lang):
-    for index in range(len(value)):
-        element = value[index]
-        # print(f"element[{index}] = {element}")
-        for replacement in front_matter_replace_rules:
-            if replacement["orginal_text"] in element:
-                # 使用 replace 函数逐个替换
-                element = element.replace(
-                    replacement["orginal_text"], replacement["replaced_text"][lang])
-        value[index] = element
-        # print(f"element[{index}] = {element}")
-    return value
-
-# 定义调用 ChatGPT API 翻译的函数
 def translate_text(text, lang, type):
-    print("Input: ", text);
+    print("Input:  \"", text, "\"");
+
+    # 开头的空格要保留，影响markdown格式
+    spaces = ""
+    match = re.match(r'^\s+', text)
+    if(match):
+        spaces = match.group(0)
+
     outputs = pipeline_ins(input=text)
-    print("Output: ", outputs['translation'])
-    return outputs['translation']
+    output = spaces + outputs['translation']
+    print("Output: \"", output, "\"")
+    return output
 
-# Front Matter 处理规则
-def translate_front_matter(front_matter, lang):
-    translated_front_matter = {}
-    for key, value in front_matter.items():
-        if key in front_matter_translation_rules:
-            processed_value = front_matter_translation_rules[key](value, lang)
-        else:
-            # 如果在规则列表内，则不做任何翻译或替换操作
-            processed_value = value
-        translated_front_matter[key] = processed_value
-        # print(key, ":", processed_value)
-    return translated_front_matter
-
-# 定义文章拆分函数
-def split_text(text, max_length):
-    # 根据段落拆分文章
-    paragraphs = text.split("\n\n")
-    output_paragraphs = []
-    current_paragraph = ""
-
-    for paragraph in paragraphs:
-        if len(current_paragraph) + len(paragraph) + 2 <= max_length:
-            # 如果当前段落加上新段落的长度不超过最大长度，就将它们合并
-            if current_paragraph:
-                current_paragraph += "\n\n"
-            current_paragraph += paragraph
-        else:
-            # 否则将当前段落添加到输出列表中，并重新开始一个新段落
-            output_paragraphs.append(current_paragraph)
-            current_paragraph = paragraph
-
-    # 将最后一个段落添加到输出列表中
-    if current_paragraph:
-        output_paragraphs.append(current_paragraph)
-
-    # 将输出段落合并为字符串
-    output_text = "\n\n".join(output_paragraphs)
-
-    return output_text
-
-# 定义翻译文件的函数
 def translate_file(input_file, filename, lang):
     print(f"Translating into {lang}: {filename}")
     sys.stdout.flush()
@@ -210,7 +53,7 @@ def translate_file(input_file, filename, lang):
         input_text = f.read()
 
     # 拆分文章
-    punctuation_pattern = re.compile('[，。！？、；：“”‘’（）《》【】#,!?;:\'"*{}()-+\n|-]')
+    punctuation_pattern = re.compile('[。！？；#!?;:\'"*{}()-+\n|-]')
     paragraphs = []
     matches = list(punctuation_pattern.finditer(input_text))
     start_index = 0
@@ -238,7 +81,7 @@ def translate_file(input_file, filename, lang):
             output_paragraphs.append(paragraph)
 
     # 将输出段落合并为字符串
-    output_text = "\n\n".join(output_paragraphs)
+    output_text = "".join(output_paragraphs)
 
     # 写入输出文件
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
@@ -259,7 +102,6 @@ try:
     if not os.path.exists(processed_list):
         with open(processed_list, "w", encoding="utf-8") as f:
             print("processed_list created")
-            sys.stdout.flush()
 
     # 遍历目录下的所有.md文件，并进行翻译
     for filename in sorted_file_list:
@@ -276,7 +118,6 @@ try:
 
             if filename in processed_list_content:  # 不进行翻译
                 print(f"Pass the post in processed_list: {filename}")
-                sys.stdout.flush()
             translate_file(input_file, filename, "en")
 
             # 将处理完成的文件名加到列表，下次跳过不处理
@@ -286,15 +127,10 @@ try:
                     f.write("\n")
                     f.write(filename)
 
-            # 强制将缓冲区中的数据刷新到终端中，使用 GitHub Action 时方便实时查看过程
-            sys.stdout.flush()
-            
     # 所有任务完成的提示
     print("Congratulations! All files processed done.")
-    sys.stdout.flush()
 
 except Exception as e:
     # 捕获异常并输出错误信息
     print(f"An error has occurred: {e}")
-    sys.stdout.flush()
     raise SystemExit(1)  # 1 表示非正常退出，可以根据需要更改退出码
